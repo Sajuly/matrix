0a6671a13c0299254c68957124aa791da79bfb21
Sajuly <107915361+Sajuly@users.noreply.github.com>
2022-07-16 17:46:48 +0300

    Update s21_matrix.c

--- s21_matrix.c
+++ s21_matrix.c
@@ -57,3 +57,18 @@ int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
     }
     return code;
 }
+
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+    int code = 0;
+    if ((A->rows == B->rows) && (A->columns == B->columns)) {
+        s21_create_matrix(A->rows, A->columns, result);
+        for (int i = 0; i < A->rows; i++) {
+            for (int j = 0; j < A->columns; j++) {
+                result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+            }
+        }
+    } else {
+        code = 2;
+    }
+    return code;
+}
