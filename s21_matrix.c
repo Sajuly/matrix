f429b0d9ee0129462d92a5aecd465beadbc3390d
Sajuly <107915361+Sajuly@users.noreply.github.com>
2022-07-16 17:16:13 +0300

    Update s21_matrix.c

--- s21_matrix.c
+++ s21_matrix.c
@@ -13,3 +13,32 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
     }
     return code;
 }
+
+void s21_remove_matrix(matrix_t *A) {
+    if (A->matrix) {
+        for (int i; i < A->rows) {
+            free(A->matrix[i]);
+        }
+        A->rows = 0;
+        A->columns = 0;
+        free(A->matrix);
+        A->matrix = NULL
+    }
+}
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+    int equality = SUCCESS;
+    if ((A->rows == B->rows) && (A->columns == B->columns)) {
+        for (int i = 0; i < A->rows; i++) {
+            for (int j= 0; j < A->columns, j++) {
+                if (fabs(A->matrix[i][j] - B->matrix[i][j]) > 0.0000001) {
+                    equality = FAILURE;
+                    break;
+                }
+            }
+        }
+    } else {
+        equality = FAILURE;
+    }
+    return equality;
+}
